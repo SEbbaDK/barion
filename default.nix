9efcd2445851f79fb906389e14d7c5a69be4b04d
SEbbaDK <sebbadk@gmail.com>
2021-10-20 00:28:18 +0200

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/d68053da0ab2e890703f6760854fe9f1d050b801.tar.gz") {}
+, buildCrystalPackage ? pkgs.crystal.buildCrystalPackage
 }:
-pkgs.crystal.buildCrystalPackage rec {
+buildCrystalPackage rec {
     pname = "barion";
     version = "0.2";
 
